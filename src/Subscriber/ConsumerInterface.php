4b6759730e7d345608829c1acfc08345bc11ec08
Órigo Energia <80329537+origoenergia@users.noreply.github.com>
2021-07-12 07:45:14 -0300

    Update receive message method parameters

--- src/Subscriber/ConsumerInterface.php
+++ src/Subscriber/ConsumerInterface.php
@@ -2,15 +2,15 @@
 
 namespace OrigoEnergia\AzureServiceBusLaravel\Subscriber;
 
+use AzureServiceBus\ServiceBus\Models\ReceiveMode;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use AzureServiceBus\ServiceBus\Models\ReceiveMessageOptions;
 use OrigoEnergia\AzureServiceBusLaravel\Destination\Message;
 use OrigoEnergia\AzureServiceBusLaravel\Common\PubSubCommonInterface;
 
 
 interface ConsumerInterface extends PubSubCommonInterface
 {
-    public function receiveMessage(?string $consumerName = null, ?string $destinationName = null, ?ReceiveMessageOptions $options = null): ?self;
+    public function receiveMessage(int $timeout = 0, int $mode = ReceiveMode::RECEIVE_AND_DELETE | ReceiveMode::PEEK_LOCK): ?self;
     public function getMessage(): Message;
     public function getUnserializedMessage(): ShouldQueue;
 }
